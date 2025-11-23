40577de1e22ea1c4c4890e055f27c10d3608311a
AX-I <36831950+AX-I@users.noreply.github.com>
2025-11-23 14:07:29 -0500

    Display message for connection error

--- union-battle/Scripts/start_scene_scripts/start_game_button.gd
+++ union-battle/Scripts/start_scene_scripts/start_game_button.gd
@@ -1,5 +1,6 @@
 extends Button
 
+const CONN_ERR_STR = 'Error connecting to server\nPlease run `python UBServer.py` outside of Godot'
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,17 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
+func push_GUI_error(msg):
+	self.get_node('ErrorLabel').text = msg
+	var ti = Timer.new()
+	add_child(ti)
+	ti.wait_time = 5
+	ti.one_shot = true
+	ti.start()
+	ti.timeout.connect(reset_GUI_error)
+
+func reset_GUI_error():
+	self.get_node('ErrorLabel').text = ''
 
 func _on_pressed() -> void:
 	var req = self.get_node('JoinRequest')
@@ -18,12 +30,19 @@ func _on_pressed() -> void:
 	Globals.SERVER_ADDR = serv
 	Globals.USERNAME = username
 	req.request_completed.connect(_on_request_completed)
-	req.request(Globals.SERVER_ADDR + '/join?user=' + Globals.USERNAME)
+	req.timeout = 1
+
+	var target = Globals.SERVER_ADDR + '/join?user=' + Globals.USERNAME
 	print('Server ', serv)
 	print('User ', username)
-
+	var err = req.request(target)
+	if err:
+		push_GUI_error(CONN_ERR_STR)
 
 func _on_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_GUI_error(CONN_ERR_STR)
+		return
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print('Server response ', json)
 	Globals.MY_ID = json['id']
