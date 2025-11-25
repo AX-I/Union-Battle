1a8f80136f7908b928adc99b945bb5f1456acc25
AX-I <36831950+AX-I@users.noreply.github.com>
2025-11-24 20:34:55 -0500

    Sync: receive card action

--- union-battle/Scripts/strike_scene_scripts/connection_in.gd
+++ union-battle/Scripts/strike_scene_scripts/connection_in.gd
@@ -57,5 +57,11 @@ func syncPlayers(data):
 				syncOnePlayer(pid, pdata)
 
 func syncOnePlayer(pid: int, pdata: Dictionary):
+	if 'actions' in pdata:
+		for act in pdata['actions']:
+			var card = PlayingCard.new_card_from_data(act['card'])
+			var targ_id = act['target']
+			Globals.PLAYERS[targ_id].adjust_stats(card)
+			card.queue_free()
 	if 'data' in pdata:
 		Globals.PLAYERS[pid].set_stats(pdata['data']['stats'])
